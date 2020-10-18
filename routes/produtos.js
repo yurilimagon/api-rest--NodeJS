a99b2ab80ff393584478119d6ee72404d34abc23
Yuri Lima Gonçalves <yurilimagoncalves@gmail.com>
2020-10-18 06:47:54 -0300

    Update rota pedidos

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,39 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+
 const login = require('../middleware/login');
 const produtosController = require('../controllers/produtos-controller');
 
-const storage = multer.diskStorage({
-    destination: function(re,file,cb){
-        cb(null, './uploads/');
-    },
-    filename: function(re,file,cb){
-        cb(null, new Date().toISOString() + file.originalname);
-    },
-});
 
-const fileFilter = (req,file,cb) => {
-    if (file.mimetipe === 'image/jpeg' || file.mimetipe === 'image/png') {
-        cb(null, true);
-    } else{
-        cb(null, false);
-    }
-}
-
-const upload = multer({ 
-    storage: storage,
-    limits: {
-        fileSize: (1024 * 1024) * 5
-    },
-    fileFilte: fileFilter
-});
 
 // RETORNA TODOS OS PRODUTOS
 router.get('/', produtosController.getProdutos);
 
 // INSERE UM PRODUTO
-router.post('/', login.obrigatorio, upload.single('produto_imagem'), produtosController.postProduto);
+router.post('/', login.obrigatorio, produtosController.postProduto);
 
 // RETORNA OS DADOS DE UM PRODUTO
 router.get('/:id_produto', produtosController.getUmProduto);
